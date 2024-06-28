4ed22cdedcceec3b9ab519ad5ebc29047cfc5c4e
ossovski3006 <135758030+ossovski3006@users.noreply.github.com>
2024-06-28 15:21:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Aternativa 2"
+    ]
+}
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+    
+    
